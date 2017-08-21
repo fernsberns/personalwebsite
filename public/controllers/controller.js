7a0f7e4fe8aff715020b4e300e071d4c3818b8e4
fernsberns6 <johnbernad6@gmail.com>
2017-08-20 21:04:03 -0700

    1st commit

---
+++ public/controllers/controller.js
@@ -0,0 +1,219 @@
+var WebApp = angular.module('WebApp', ['ngRoute']);
+
+
+
+WebApp.config(['$routeProvider', function($routeProvider){
+
+	$routeProvider
+	.when('/',{
+		templateUrl: 'views/landingpage.html'
+	})
+	.when('/home',{
+		resolve: {
+			"check": function($location, $rootScope){
+				if($rootScope.loggedIn == true){
+					$location.path('/home');
+				}
+				else{
+					$location.path('/');
+				}
+			}
+		},
+		templateUrl: 'views/home.html',
+		controller: 'loginCtrl'
+	})
+	.when('/messages',{
+		resolve: {
+			"check": function($location, $rootScope){
+				if($rootScope.loggedIn == true){
+					$location.path('/messages');
+				}
+				else{
+					$location.path('/');
+				}
+			}
+		},
+		templateUrl: 'views/homemessages.html',
+		controller: 'ChatController'
+	})
+	.when('/login',{
+		templateUrl: 'views/login.html',
+		controller: 'loginCtrl'
+	})
+	.when('/signup',{
+		templateUrl: 'views/signup.html'
+	})
+	.when('/contactus',{
+		templateUrl: 'views/contactus.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+
+
+
+}]);
+
+
+
+//logincontroller
+
+WebApp.controller('loginCtrl', function($scope, $location,$rootScope){
+	$scope.submit = function(){
+	if($scope.email == 'admin' && $scope.password == 'pass'){
+		$rootScope.loggedIn = true;
+		$location.path('/home');
+		}
+	else{
+		alert('invalid credentials');
+		};
+	};
+	$scope.logout = function(){
+		$rootScope.loggedIn = false;
+	};
+
+	
+});
+
+
+//chatcontroller
+
+WebApp.controller('ChatController', ['$scope', '$http', function($scope, $http) {
+	console.log("Hello World from controller");
+
+var socket = io.connect('http://localhost:3000');
+
+var myVar;
+
+
+
+var refresh=function(){
+	$http.get('/listofmessages').then(function(response){
+		console.log("I got the data I requested");
+		$scope.listofmessages = response.data;
+		$scope.messages=null;
+		$( '#messageBody' ).animate({scrollTop:9999999999999999999},500);
+    	$( '#loginBody' ).animate({scrollTop:9999999999999999999},500);
+	});
+
+};
+
+var refresh3=function(){
+	$http.get('/listofmessages').then(function(response){
+		console.log("I got the data I requested");
+		$scope.listofmessages = response.data;
+	});
+
+};
+var refresh2=function(){
+	$http.get('/listofusers').then(function(response){
+		console.log("I got the data I requested");
+		$scope.listofusers = response.data;
+		$scope.users=null;
+	});
+
+};
+
+
+refresh();
+refresh2();
+
+myVar = setInterval(alertFunc, 1000);
+
+function alertFunc() {
+  
+	refresh3();
+
+	refresh2();
+
+}
+
+
+
+$scope.addMessage= function(){
+	console.log($scope.messages);
+	$http.post('/listofmessages', $scope.messages).then(function(response){
+		console.log(response);
+		refresh();
+		socket.emit('refresh');
+	});
+};
+
+$scope.addUser= function(){
+	console.log($scope.users);
+
+	$http.post('/listofusers', $scope.users).then(function(response){
+		console.log(response);
+		refresh2();
+		socket.emit('refresh2');
+	});
+};
+
+
+
+
+socket.on('refresh',function(){
+	refresh();
+});
+
+socket.on('refresh2',function(){
+	refresh2();
+});
+
+
+socket.on('userdisconnect',function(){
+		removeUser();
+});
+
+
+
+
+$scope.removeUser=function(id){
+	console.log(id);
+	$http.delete('/listofusers/'+id).then(function(response){
+		refresh2();
+	});
+};
+
+$scope.removeScore=function(id){
+	console.log(id);
+	$http.delete('/listofmessages/'+id).then(function(response){
+		refresh();
+	});
+};
+
+$scope.editScore=function(id){
+	console.log(id);
+	$http.get('/listofmessages/'+id).then(function(response){
+		$scope.messages=response.data;
+	});
+};
+
+$scope.updateScore=function(){
+	console.log($scope.messages._id);
+	$http.put('/listofmessages/'+$scope.messages._id,$scope.scor).then(function(response){
+		refresh();
+	});
+};
+
+(function myLoop (i) {          
+   setTimeout(function () {   
+      gettmstmp();            
+      if (--i) myLoop(i);     
+   }, 1000)
+})(999999999999999999999999);   
+
+
+$scope.example = new Date();
+     
+
+     function gettmstmp() {
+     var tmstmp = new Date();
+     document.getElementById("tmstmp").value=Date();
+ 	
+
+ 	$scope.example = tmstmp;
+
+	}
+
+}]);
---
+++ server.js
@@ -0,0 +1,135 @@
+var express = require('express');
+var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
+var socket = require('socket.io');
+var mLab = require('mongolab-data-api')('z127-aeTjCC6pmYw0HgMBkVTYrutkJiS');
+
+
+var app = express();
+
+app.use(express.static(__dirname+"/public"));
+app.get('/', function(req, res){
+    res.sendfile('index.html', { root: __dirname + "/public" } );
+});
+app.use(bodyParser.json());
+
+var db = mongojs('mongodb://admin:pass@ds035766.mlab.com:35766/chatdb', ['listofmessages']);
+var db2 = mongojs('mongodb://admin:pass@ds035766.mlab.com:35766/chatdb', ['listofusers']);
+
+//db 
+
+app.get('/views/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('views/' + name);
+});
+
+app.get('/listofmessages', function(req, res){
+	console.log("Receive a GET request")
+
+	db.listofmessages.find(function(err,docs){
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+app.post('/listofmessages', function(req, res){
+	console.log(req.body);
+	db.listofmessages.insert(req.body, function(err, doc){
+		res.json(doc);
+	});
+});
+
+app.delete('/listofmessages/:id',function(req,res){
+	var id=req.params.id;
+	console.log(id);
+	db.listofmessages.remove({_id:mongojs.ObjectId(id)},function(err,doc){
+		res.json(doc);
+	});
+});
+
+app.get('/listofmessages/:id',function(req,res){
+	var id=req.params.id;
+	console.log(id);
+	db.listofmessages.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
+		res.json(doc);
+	});
+});
+
+//db2
+
+app.get('/listofusers', function(req, res){
+	console.log("Receive a GET request")
+
+	db2.listofusers.find(function(err,docs){
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+app.post('/listofusers', function(req, res){
+	console.log(req.body);
+	db2.listofusers.insert(req.body, function(err, doc){
+		res.json(doc);
+	});
+});
+
+app.delete('/listofusers/:id',function(req,res){
+	var id=req.params.id;
+	console.log(id);
+	db2.listofusers.remove({_id:mongojs.ObjectId(id)},function(err,doc){
+		res.json(doc);
+	});
+});
+
+app.get('/listofusers/:id',function(req,res){
+	var id=req.params.id;
+	console.log(id);
+	db2.listofusers.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
+		res.json(doc);
+	});
+});
+
+
+//app.put('/listofmessages/:id',function(req,res){
+//	var id=req.params.id;
+//	console.log(req.body.LastName);
+//	db.listofmessages.findAndModify({query:{_id:mongojs.ObjectId(id)},
+//		update:{$set:{FirstName:req.body.FirstName,LastName:req.body.LastName,DName:req.body.DName,DateTime:req.body.DateTime}},
+//		new:true},function(err,doc){
+//			res.json(doc);
+//		});
+//});
+
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function() {
+console.log('Company server running on port 3000');
+});
+
+var io = socket(server);
+
+io.on('connection', function(socket){
+	console.log('made socket connection',socket.id)
+
+
+
+	socket.on('refresh',function(){
+
+		io.sockets.emit('refresh');
+	});
+
+	socket.on('refresh2',function(){
+
+		io.sockets.emit('refresh2');
+	});
+
+	socket.on('disconnect', function(){
+    console.log('user disconnected');
+    	io.sockets.emit('userdisconnect');
+
+  });
+
+	
+
+});
+
